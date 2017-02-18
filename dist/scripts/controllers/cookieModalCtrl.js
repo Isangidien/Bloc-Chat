85f8896de1feab2267df6025c09b117a5a4d6d86
isangidien <i.inessa.miller@gmail.com>
2017-02-17 19:36:24 -0500

    user story 5 completed

---
+++ dist/scripts/controllers/cookieModalCtrl.js
@@ -0,0 +1,37 @@
+(function (){
+    function cookieModalCtrl ($uibModalInstance,$cookies) {
+        this.userNameInput;
+        this.error;
+        /**
+        *@function this.setUserName
+        *@desc public function that sets a username to a cookie
+        *@param {string} userNameInput
+        **/
+        this.setUserName = function (userNameInput,elem) {
+            if(this.userNameInput && this.userNameInput !=""){
+              
+          
+           $cookies.put('userName',this.userNameInput);
+           this.closeModal();
+            } else {
+                this.error = "Hey, John or Jane Doe, fill in the blank";
+            }
+        }
+        
+        this.closeModal = function () {
+            $uibModalInstance.close();
+            
+            //checks if cookie holds the username
+            var theCookie = $cookies.get('userName');
+             console.log(theCookie);
+
+        }
+        
+        
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('cookieModalCtrl',['$uibModalInstance','$cookies', cookieModalCtrl]);
+    
+})();
